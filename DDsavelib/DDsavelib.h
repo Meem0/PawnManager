1790a83bcfe4caa00befa5875b42c1b07896c100
Meem0 <themeem0@gmail.com>
2016-02-10 21:31:26 -0800

    Rename DDsavetool -> DDsavelib

--- DDsavelib/DDsavelib.h
+++ DDsavelib/DDsavelib.h
@@ -1,8 +1,6 @@
 #pragma once
 
-extern "C" __declspec(dllexport) int Unpack(const char *path);
-
-extern "C" __declspec(dllexport) int Validate(const char *path);
+extern "C" __declspec(dllexport) int Unpack(const char *path, unsigned char *output);
 
 extern "C" __declspec(dllexport) int ERR_READ;
 extern "C" __declspec(dllexport) int ERR_WRITE;
